4064f3a7322c2ea2b70c97481c5ccb71a52307d5
roshansaini18 <135322753+roshansaini18@users.noreply.github.com>
2025-08-05 19:09:06 +0530

    Update index.jsx

--- components/Customer/index.jsx
+++ components/Customer/index.jsx
@@ -5,7 +5,7 @@ import useSWR from "swr";
 import {fetchData} from "../../modules/modules";
 const CustomerDashboard =() => {
   //get userInfo from sessionStorage
-  const userInfo=JSON.parse(localStorage.getItem("userInfo"));
+  const userInfo=JSON.parse(sessionStorage.getItem("userInfo"));
   const {data:trData,error:trError}=useSWR(
     `/api/transaction/summary?accountNo=${userInfo.accountNo}`,
     fetchData,
